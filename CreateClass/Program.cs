f5c07d07dbc5f142f82e39f3aa8188e70a9df022
bozsob <bozso.beatrix@gmail.com>
2017-05-31 09:31:21 +0200

    Exercise2: Inheritance

--- CreateClass/Program.cs
+++ CreateClass/Program.cs
@@ -9,14 +9,13 @@ namespace CreateClass
     class Person
     {
         private string name;
-        private DateTime birthDate;
-
         public string Name
         {
             get { return name; }
             set { name = value; }
         }
 
+        private DateTime birthDate;
         public DateTime BirthDate
         {
             get { return birthDate; }
@@ -36,5 +35,58 @@ namespace CreateClass
             return String.Format("Name: {0}, Birth date: {2}", this.name, this.birthDate);
         }
     }
+
+
+    class Employee : Person
+    {
+        public Room room;
+        private int salary;
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        private string profession;
+        public string Profession
+        {
+            get { return profession; }
+            set { profession = value; }
+        }
+
+        public Employee()
+        {
+            room = null;
+        }
+
+        public Employee(string name, DateTime birthDate, int salary, string profession): base(name, birthDate)
+        {
+            this.salary = salary;
+            this.profession = profession;
+            room = null;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + String.Format(", Salary: {0}, Profession: {1}, Room: {2}", salary, profession, room.Number);
+        }
+    }
+
+
+    class Room
+    {
+        private int number;
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public Room(int number)
+        {
+            this.number = number;
+        }
+    }
+
 }
 
